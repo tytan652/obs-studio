32dcd27116807964e83143793a1f5441fe521fc9
tytan652 <tytan652@tytanium.xyz>
2023-04-23 13:16:45 +0200

    obs-outputs, reuse: Make librtmp REUSE compliant
    This commit does not make the whole repo REUSE compliant.

--- plugins/obs-outputs/librtmp/handshake.h
+++ plugins/obs-outputs/librtmp/handshake.h
@@ -1,25 +1,10 @@
 /*
- *  Copyright (C) 2008-2009 Andrej Stepanchuk
- *  Copyright (C) 2009-2010 Howard Chu
- *  Copyright (C) 2010 2a665470ced7adb7156fcef47f8199a6371c117b8a79e399a2771e0b36384090
+ * Copyright (C) 2008-2009 Andrej Stepanchuk
+ * Copyright (C) 2009-2010 Howard Chu
+ * Copyright (C) 2010 2a665470ced7adb7156fcef47f8199a6371c117b8a79e399a2771e0b36384090
+ * SPDX-FileCopyrightText: 2014, 2016, 2018, 2020-2021 OBS Studio Contributors
  *
- *  This file is part of librtmp.
- *
- *  librtmp is free software; you can redistribute it and/or modify
- *  it under the terms of the GNU Lesser General Public License as
- *  published by the Free Software Foundation; either version 2.1,
- *  or (at your option) any later version.
- *
- *  librtmp is distributed in the hope that it will be useful,
- *  but WITHOUT ANY WARRANTY; without even the implied warranty of
- *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
- *  GNU General Public License for more details.
- *
- *  You should have received a copy of the GNU Lesser General Public License
- *  along with librtmp see the file COPYING.  If not, write to
- *  the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
- *  Boston, MA  02110-1301, USA.
- *  http://www.gnu.org/copyleft/lgpl.html
+ * SPDX-License-Identifier: LGPL-2.1-or-later
  */
 
 /* This file is #included in rtmp.c, it is not meant to be compiled alone */
--- plugins/obs-outputs/librtmp/rtmp_sys.h
+++ plugins/obs-outputs/librtmp/rtmp_sys.h
@@ -1,26 +1,12 @@
+/*
+ * Copyright (C) 2010 Howard Chu
+ * SPDX-FileCopyrightText: 2014-2015, 2018, 2020 OBS Studio Contributors
+ *
+ * SPDX-License-Identifier: LGPL-2.1-or-later
+ */
+
 #ifndef __RTMP_SYS_H__
 #define __RTMP_SYS_H__
-/*
- *      Copyright (C) 2010 Howard Chu
- *
- *  This file is part of librtmp.
- *
- *  librtmp is free software; you can redistribute it and/or modify
- *  it under the terms of the GNU Lesser General Public License as
- *  published by the Free Software Foundation; either version 2.1,
- *  or (at your option) any later version.
- *
- *  librtmp is distributed in the hope that it will be useful,
- *  but WITHOUT ANY WARRANTY; without even the implied warranty of
- *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
- *  GNU General Public License for more details.
- *
- *  You should have received a copy of the GNU Lesser General Public License
- *  along with librtmp see the file COPYING.  If not, write to
- *  the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
- *  Boston, MA  02110-1301, USA.
- *  http://www.gnu.org/copyleft/lgpl.html
- */
 
 #include <stdio.h>
 #include <stdlib.h>
