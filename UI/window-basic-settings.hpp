5764f671522b32eaa03f6edef0e1f29995c3e719
tytan652 <tytan652@tytanium.xyz>
2023-06-15 21:12:19 +0200

    Revert "UI: Enable WHIP service in UI"
    This reverts commit:
    - d31d271b43ed6a119fca4499431388b296ad5e75
    - 353a4c860ddd2077aa2ab970964a23565d42bb12
    - dd392188b8f60ebc7e600ea803eda5304b4aebf9

--- UI/window-basic-settings.hpp
+++ UI/window-basic-settings.hpp
@@ -251,7 +251,6 @@ private:
 	/* stream */
 	void InitStreamPage();
 	inline bool IsCustomService() const;
-	inline bool IsWHIP() const;
 	void LoadServices(bool showAll);
 	void OnOAuthStreamKeyConnected();
 	void OnAuthConnected();
